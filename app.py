cd04219f6fe3917538b6db65d2dff886498912c3
hyeong3419 <taeh3419@gmail.com>
2024-12-11 15:31:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -218,13 +218,15 @@ def get_factcheck_response(query):
         
 def get_connection():
     return pymysql.connect(
-        host='localhost',
-        user='root',
-        password='135790',
-        db='capstone',
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        db=os.getenv('DB_NAME'),
+        port=int(os.getenv('DB_PORT', '3306')),
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
+
     
 def get_feedback_for_query(query):
     feedbacks = []
